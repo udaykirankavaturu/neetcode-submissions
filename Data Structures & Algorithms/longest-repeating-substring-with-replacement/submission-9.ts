77d225419b93f19f16851a248b23ed8d1d374658
Uday Kiran Kavaturu <72560240+udaykirankavaturu@users.noreply.github.com>
2026-08-13 14:03:34 +0530

    Add: longest-repeating-substring-with-replacement - submission-9

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-9.ts
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let res = 0;
+        let charSet = new Set(s);
+
+        for (let c of charSet) {
+            let count = 0,
+                l = 0;
+
+            // expand
+            for (let r = 0; r < s.length; r++) {
+                if (s[r] === c) {
+                    count++;
+                }
+
+                // shrink
+                while (r - l + 1 - count > k) {
+                    if (s[l] === c) {
+                        count--;
+                    }
+                    l++;
+                }
+
+                res = Math.max(res, r - l + 1);
+            }
+        }
+        return res;
+    }
+}
